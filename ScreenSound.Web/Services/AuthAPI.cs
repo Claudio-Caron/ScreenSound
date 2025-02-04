ed89dbc77f742d28608bd0d7eb243864d4592c27
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2025-02-04 16:37:37 -0400

    Verificar estado de autenticação / notificar a aplicação da mudança de estado / implementar e condicionar endpoint de logout

--- ScreenSound.Web/Services/AuthAPI.cs
+++ ScreenSound.Web/Services/AuthAPI.cs
@@ -7,10 +7,12 @@ namespace ScreenSound.Web.Services
 {
     public class AuthAPI(IHttpClientFactory factory):AuthenticationStateProvider
     {
+        private bool autenticado = false;
         private readonly HttpClient _httpClient= factory.CreateClient("API");
 
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
+            autenticado = false;
             var pessoa = new ClaimsPrincipal();
             var response = await _httpClient.GetAsync("auth/manage/info");
             if (response.IsSuccessStatusCode)
@@ -23,6 +25,7 @@ namespace ScreenSound.Web.Services
                     ];
                 var identity = new ClaimsIdentity(dados, "Cookies");
                 pessoa = new ClaimsPrincipal(identity);
+                autenticado = true;
             }
             return new AuthenticationState(pessoa);
         }
@@ -32,7 +35,7 @@ namespace ScreenSound.Web.Services
             var response = await _httpClient.PostAsJsonAsync("auth/login?useCookies=true", new
             {
                 email,
-                password = senha
+                password = senha 
             });
             if (response.IsSuccessStatusCode)
             {
@@ -44,6 +47,16 @@ namespace ScreenSound.Web.Services
                 Success = false,
                 Errors = "Email ou senha invalidos"
             };
+        }
+        public async Task LogoutAsync()
+        {
+            await _httpClient.PostAsync("auth/logout", null);
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+        public async Task<bool> VerificaAutenticado()
+        {
+            await GetAuthenticationStateAsync();
+            return autenticado;
         }
     }
 }
